765cb6c5cec5320bfb1b194726912b096d07559a
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2024-11-22 12:06:52 +0530

    Dashboard updatedd

---
+++ .history/script_home_20241122111802.js
@@ -0,0 +1,21 @@
+// import { Clerk } from '@clerk/clerk-js'
+
+// const pubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+// const clerk = new Clerk(pubKey)
+// await clerk.load()
+
+// if (clerk.user) {
+//   document.getElementById('logout').addEventListener('click', async () => {
+//     await clerk.signOut()
+//     window.location.reload();
+//   })
+// }
+
+function startDailyChallenge() {
+    window.location.href = "./quizz/index.html";
+}
+
+function startTest(difficulty) {
+    window.location.href = "./quizz/index.html";
+}
